984bb4b7a50ecc921f9e4c124a40377e84a28e20
tum <t.songpon@gmail.com>
2016-08-28 22:06:01 +0700

    Re-structure project

--- app/api_1_0/todo.py
+++ app/api_1_0/todo.py
@@ -1,26 +1,31 @@
 from flask import Flask
 from flask import jsonify
-from models import User, Todo
+from . import api
+from ..models import User, Todo
 import json
-app = Flask(__name__)
 
-@app.route('/')
+@api.route('/')
 def hello_world():
     return 'Flask Dockerized'
 
-@app.route('/todos/')
+@api.route('/todos/')
 def list_todos():
     todo_1 = Todo(1, 'Buy milk', 'Buy 2 of buttles of milk')
     todo_2 = Todo(2, 'Buy pork', 'Buy 2 kg. of pork')
-    todos = [todo_1, todo_2]
+    todo_3 = Todo(3, 'Learn python', 'Learn python programming')
+    todos = [todo_1, todo_2, todo_3]
     return jsonify([each.to_json() for each in todos])
 
-@app.route('/users/')
+@api.route('/todos/<int:id>')
+def get_todo(id):
+    toto = Todo(id, 'Learn python', 'Learn python programming');
+    response = jsonify(toto.to_json())
+    response.add_etag()
+    return response
+
+@api.route('/users/')
 def get_user():
     user_songpon = User(1, 'Songpon', 't.songpon@gmail.com', 30)
     user_tum = User(2, 'Tum', 'tum@songpon.net', 25)
     users = [user_songpon, user_tum]
     return jsonify([each_user.to_json() for each_user in users])
-
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
---
+++ manage.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
+if os.path.exists('.env'):
+    print('Importing environment from .env...')
+    for line in open('.env'):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
+from app import create_app
+from app.models import User, Todo
+from flask_script import Manager, Shell
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+
+
+def make_shell_context():
+    return dict(app=app, User=User, Todo=Todo)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+
+if __name__ == '__main__':
+    manager.run()
